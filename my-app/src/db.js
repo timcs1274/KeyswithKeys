957cca93aa4121198fd6f1132cf4d0f699c7d863
timcs <timcs1274@gmail.com>
2023-01-27 13:58:32 -0500

    troubleshoot db

--- my-app/src/db.js
+++ my-app/src/db.js
@@ -6,3 +6,11 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     dialect: 'mysql',
   });
   
+  sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
