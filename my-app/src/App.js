3f0e94bced2ab7fba6d03bf2314e7a764aca84cf
timcs <timcs1274@gmail.com>
2023-02-09 12:52:54 -0500

    update routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import Signup from './pages/Signup';
+import User from './pages/User';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route> path="/Home/" element={<Home />}</Route>
+          <Route> path="/Signup" element={<Signup />}</Route>
+          <Route> path="/User/:id" element={<User />}</Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
